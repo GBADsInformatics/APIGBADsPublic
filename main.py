d48d616fa59fb88f4d0a1cd8fdc9ecfa25317a8c
gbadske <dastacey@gbadske.org>
2023-08-24 16:31:22 -0400

    change to Slackbot approve - revert

--- main.py
+++ main.py
@@ -498,26 +498,26 @@ async def slack_approve_comment(comment_id: str, authorization_token: str, revie
     #
     # Extract information from the json file and construct a database table entry
     #
-    bucket = "gbads-comments"
-    srcFolder = "underreview/"
-    key0 = srcFolder+comment_id
-    json_object = s3_client.get_object(Bucket=bucket,Key=key0)
-    file_reader = json_object['Body'].read().decode("utf-8")
-    file_reader = json.loads(file_reader)
-    created = str(file_reader["created"])[0:19]
-    approved = str(datetime.datetime.now())[0:19]
-    dashboard = str(file_reader["dashboard"])
-    table = str(file_reader["table"])
-    subject = str(file_reader["subject"])
-    message = str(file_reader["message"])
-    isPublic = str(file_reader["isPublic"]).upper()
-    if isPublic == "FALSE":
-        name = "NULL"
-        email = "NULL"
-    else:
-        name = str(file_reader["name"])
-        email = str(file_reader["email"])
-    dbRow = "('"+created+"','"+approved+"','"+dashboard+"','"+table+"','"+subject+"','"+message+"','"+name+"','"+email+"',"+isPublic+",'"+reviewer+"')"
+#    bucket = "gbads-comments"
+#    srcFolder = "underreview/"
+#    key0 = srcFolder+comment_id
+#    json_object = s3_client.get_object(Bucket=bucket,Key=key0)
+#    file_reader = json_object['Body'].read().decode("utf-8")
+#    file_reader = json.loads(file_reader)
+#    created = str(file_reader["created"])[0:19]
+#    approved = str(datetime.datetime.now())[0:19]
+#    dashboard = str(file_reader["dashboard"])
+#    table = str(file_reader["table"])
+#    subject = str(file_reader["subject"])
+#    message = str(file_reader["message"])
+#    isPublic = str(file_reader["isPublic"]).upper()
+#    if isPublic == "FALSE":
+#        name = "NULL"
+#        email = "NULL"
+#    else:
+#        name = str(file_reader["name"])
+#        email = str(file_reader["email"])
+#    dbRow = "('"+created+"','"+approved+"','"+dashboard+"','"+table+"','"+subject+"','"+message+"','"+name+"','"+email+"',"+isPublic+",'"+reviewer+"')"
     #
     # Get database information
     #
