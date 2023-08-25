82577af57b056880d2206a2ad6e8a52df60507d3
gbadske <dastacey@gbadske.org>
2023-08-25 15:41:38 -0400

    slackbot - stages 1, 2, and 4

--- main.py
+++ main.py
@@ -502,6 +502,7 @@ async def slack_approve_comment(comment_id: str, authorization_token: str, revie
     # Extract information from the json file and construct a database table entry
     #
     bucket = "gbads-comments"
+    srcFolder = "underreview/"
     key0 = srcFolder+comment_id
     htmlstring = "<html><body><H2>Slackbot</h2><ul><li>stage 2 - json retrieved and loaded</li>"
     json_object = s3_client.get_object(Bucket=bucket,Key=key0)
