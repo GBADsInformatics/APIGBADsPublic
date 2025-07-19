e8deae54d0430167589579f0719d9b3d4c4950cb
William Fitzjohn <78922151+WilliamFitzjohn@users.noreply.github.com>
2025-07-18 23:10:42 -0400

    Update test_main_api.py

--- tests/test_main_api.py
+++ tests/test_main_api.py
@@ -1,9 +1,32 @@
-from fastapi.testclient import TestClient
-from app.main import router
+# tests/test_main.py
 
-client = TestClient(router)
+import pytest
+from httpx import AsyncClient
+from app.main import app
 
-
-def test_dataportal_endpoint():
-    response = client.get("/dataportal/")
+@pytest.mark.asyncio
+async def test_root_get():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/")
     assert response.status_code == 200
+    assert response.json() == {"message": "Welcome to the public GBADs database tables!"}
+
+@pytest.mark.asyncio
+async def test_root_head():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.head("/")
+    assert response.status_code == 200
+
+@pytest.mark.asyncio
+async def test_docs_available():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/docs")
+    assert response.status_code == 200
+    assert "text/html" in response.headers["content-type"]
+
+@pytest.mark.asyncio
+async def test_openapi_json():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/openapi.json")
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "application/json"
